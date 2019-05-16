6631a424e033fc62cc74d30f076d6bd5713793a0
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2019-05-15 23:26:03 -0600

    fix de ls

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 set GIT_TERMINAL_PROMPT=0
 for j in $(ls -d */) ; do
     cd $j
-    if [ -d $j/.git ] ; then
+    if [ -d ./.git ] ; then
         echo Repositorio en $j:
         git fetch --all
         git pull
